a552b1720cc77f82c847dfd05c973f3889e34ae1
Scott <sfitzpatrick55@gmail.com>
2022-05-27 12:13:11 -0400

    Add GET and POST Restaurant Create to Resturant Controller

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestaurantRaterMVC.Data;
+using RestaurantRaterMVC.Models.Restaurant;
 using SwapiMVC.Models.Restaurant;
 
 namespace RestaurantRaterMVC.Controllers
@@ -25,6 +26,31 @@ namespace RestaurantRaterMVC.Controllers
             }).ToListAsync();
 
             return View(restaurants);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RestaurantCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.errorMessage = ModelState.FirstOrDefault().Value.Errors.FirstOrDefault().ErrorMessage;
+                return View(model);
+            }
+            Restaurant restaurant = new Restaurant()
+            {
+                Name = model.Name,
+                Location = model.Location,
+            };
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
