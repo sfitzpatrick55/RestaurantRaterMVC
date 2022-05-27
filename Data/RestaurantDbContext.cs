bdf2fe5c789f5b7e22ddea09027b9bbb8bd06098
Scott <sfitzpatrick55@gmail.com>
2022-05-26 21:13:08 -0400

    Create RestaurantDbContext

--- Data/RestaurantDbContext.cs
+++ Data/RestaurantDbContext.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestaurantRaterMVC.Data
 {
-    public class RestaurantDbContext
+    public class RestaurantDbContext : DbContext
     {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) {}
         
+        public DbSet<Restaurant> Restaurants { get; set; }
+        
+        public DbSet<Rating> Ratings { get; set; }
+
     }
 }
