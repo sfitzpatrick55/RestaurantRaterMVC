3b145403401072768b30b749647df7d99d818670
Scott <sfitzpatrick55@gmail.com>
2022-05-27 11:06:22 -0400

    Add async task Index to RestaurantController

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestaurantRaterMVC.Data;
+using SwapiMVC.Models.Restaurant;
 
 namespace RestaurantRaterMVC.Controllers
 {
@@ -9,6 +11,20 @@ namespace RestaurantRaterMVC.Controllers
         public RestaurantController(RestaurantDbContext context) // Constructor 
         {
             _context = context;
+        }
+
+        public async Task<IActionResult> Index() {
+
+            List<RestaurantListItem> restaurants = await _context.Restaurants
+            .Include(r => r.Ratings)
+            .Select(r => new RestaurantListItem()
+            {
+                Id = r.Id,
+                Name = r.Name,
+                Score = r.Score,
+            }).ToListAsync();
+
+            return View(restaurants);
         }
     }
 }
