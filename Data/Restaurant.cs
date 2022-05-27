b87a6c7a568c5128852163428601c718bce5719a
Scott <sfitzpatrick55@gmail.com>
2022-05-26 21:21:54 -0400

    Create Restaurant Data model

---
+++ Data/Restaurant.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Data
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Location { get; set; }
+
+        public double Score
+        {
+            get
+            {
+                return Ratings.Count > 0 ? Ratings.Select(r => r.Score).Sum() / Ratings.Count : 0;
+            }
+        }
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
+    }
+}
