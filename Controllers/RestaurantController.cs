d1d5bad7fe8045c95ecf7b3bb5e67dedee529813
Scott <sfitzpatrick55@gmail.com>
2022-05-26 21:46:04 -0400

    Add constructor and injection to Restaurant Controller

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using RestaurantRaterMVC.Data;
 
 namespace RestaurantRaterMVC.Controllers
 {
     public class RestaurantController : Controller
     {
-        
+        private RestaurantDbContext _context; // Injection
+        public RestaurantController(RestaurantDbContext context) // Constructor 
+        {
+            _context = context;
+        }
     }
 }
